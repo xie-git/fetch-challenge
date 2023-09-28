e5d621d652b7724b492f25dcd0e78211a947792f
Michael Xie <michaelxie@xie-macbook-pro.local>
2023-09-28 10:06:53 -0500

    Initial commit

---
+++ src/main/java/com/fetchchallenge/fetchchallenge/FetchchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.fetchchallenge.fetchchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchchallengeApplication.class, args);
+	}
+
+}
